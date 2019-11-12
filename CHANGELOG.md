78b1448e06dc2c665240b9eebeb8e975b888ae86
acoulton <andrew@ingenerator.com>
2019-11-12 12:15:29 +0000

    Add Base64Url string encoding helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ### Unreleased
 
+* Add Base64Url StringEncoding helper class - like base64, but with entirely websafe characters for URLs etc
 * Add JSON StringEncoding helper class - safe json parsing, encoding and prettifying with sane defaults
 * Add DeploymentConfig sub-package for loading (and, optionally, decrypting) runtime environment configuration
 
---
+++ test/unit/StringEncoding/Base64UrlTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace test\unit\Ingenerator\PHPUtils\StringEncoding;
+
+
+use PHPUnit\Framework\TestCase;
+use Ingenerator\PHPUtils\StringEncoding\Base64Url;
+
+class Base64UrlTest extends TestCase
+{
+
+    public function provider_encode_decode()
+    {
+        return [
+            ['1'],
+            ['foo'],
+            [\random_bytes(55)],
+        ];
+    }
+
+    /**
+     * @dataProvider provider_encode_decode
+     */
+    public function test_it_can_encode_and_decode_a_value($original)
+    {
+        $this->assertSame($original, Base64Url::decode(Base64Url::encode($original)));
+    }
+
+    /**
+     * @dataProvider provider_encode_decode
+     */
+    public function test_its_encoded_output_is_always_url_safe($value)
+    {
+        $encoded = Base64Url::encode($value);
+        $this->assertInternalType('string', $encoded);
+        $this->assertRegExp('/^[a-zA-Z0-9\-\_]+$/', $encoded);
+    }
+
+}
+
