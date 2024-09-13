5243b4ba86a88f994b53cf786c8d089d0445cc90
acoulton <andrew@ingenerator.com>
2024-09-13 12:05:04 +0100

    Internally refactor JSON::encode and ::decode to use JSON_THROW_ON_ERROR
    Still want to throw our own custom exception for BC, but might as well
    use the native flag to catch and rethrow JSONException rather than
    asserting against json_last_error_message manually.

--- src/StringEncoding/JSON.php
+++ src/StringEncoding/JSON.php
@@ -3,7 +3,7 @@
 namespace Ingenerator\PHPUtils\StringEncoding;
 
 use Ingenerator\PHPUtils\StringEncoding\InvalidJSONException;
-use function json_last_error_msg;
+use JsonException;
 
 class JSON
 {
@@ -16,13 +16,11 @@ class JSON
             throw new InvalidJSONException('Invalid JSON: Cannot decode a null value');
         }
 
-        $result = json_decode($json, TRUE);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new InvalidJSONException('Invalid JSON: '.json_last_error_msg());
+        try {
+            return json_decode($json, associative: true, flags: JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw new InvalidJSONException('Invalid JSON: '.$e->getMessage());
         }
-
-        return $result;
     }
 
     public static function decodeArray(string $json): array
@@ -40,7 +38,6 @@ class JSON
      * @param bool $escaped_slashes defaults true to match the PHP default
      *
      * @return string
-     * @throws \Ingenerator\PHPUtils\StringEncoding\InvalidJSONException
      */
     public static function encode(
         $value,
@@ -51,13 +48,15 @@ class JSON
             ($pretty ? JSON_PRETTY_PRINT : 0)
             |
             ($escaped_slashes ? 0 : JSON_UNESCAPED_SLASHES)
+            |
+            JSON_THROW_ON_ERROR
         );
 
-        $json = json_encode($value, $flags);
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new \Ingenerator\PHPUtils\StringEncoding\InvalidJSONException('Could not encode as JSON : ' . json_last_error_msg());
+        try {
+            return json_encode($value, $flags);
+        } catch (JsonException $e) {
+            throw new InvalidJSONException('Could not encode as JSON: '.$e->getMessage());
         }
-        return $json;
     }
 
     /**
